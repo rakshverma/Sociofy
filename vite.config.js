f2af7aa770e3c3c440e95e4e80e729edc1d67870
rakshverma <146798265+rakshverma@users.noreply.github.com>
2025-04-05 15:41:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import babel from 'vite-plugin-babel';
-import micromatch from 'micromatch';
+
 
 export default defineConfig({
   plugins: [
     react(),
-    babel({
-      babelConfig: {
-        presets: ['@babel/preset-react'], // 👈 Enables JSX support
-        plugins: [['transform-remove-console', { exclude: ['error', 'warn'] }]], // 👈 Removes console.log
-      },
-      filter: (id) =>
-        micromatch.isMatch(id, '**/*.{js,jsx,ts,tsx}') &&
-        !micromatch.isMatch(id, 'node_modules/**'),
-    }),
+   
   ],
   server: {
     proxy: {
